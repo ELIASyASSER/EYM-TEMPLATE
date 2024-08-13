20cb3c97e2cdd18dead3a6145d06045c774ef6d1
elias <eliasyasser193@gmail.com>
2024-08-13 22:21:33 +0300

    eym termp

---
+++ main.js
@@ -0,0 +1,5 @@
+let mega = document.querySelector(".mega-menu")
+let otherlinks = document.querySelector("header .ls .other")
+otherlinks.onclick = function () {
+    mega.classList.toggle("show")
+}
